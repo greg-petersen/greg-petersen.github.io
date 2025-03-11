90c570d5ed5e45f3b71e04089a8afb48a8bd6611
Greg <gregpetersen10@gmail.com>
2025-03-10 18:39:35 -0700

    ITS WORKING.PNG

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonButton } from '@ionic/angular';
+import * as moment from 'moment';
+import { interval, pipe, map, timer, timeInterval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
@@ -9,6 +11,62 @@ import { IonButton } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor() { }
+  protected leftBoobTimerOn = false;
+  protected rightBoobTimerOn = false;
 
+  protected initialStartTime?: Date;
+  protected endTime?: Date;
+
+  protected leftBoobElapsedSeconds: number = 0;
+  protected rightBoobElapsedSeconds: number = 0;
+
+  protected leftBoobSubscription?: Subscription;
+  protected rightBoobSubscription?: Subscription;
+
+  constructor() {
+  }
+
+  startLeftBoobTimer(): void {
+    if (!this.initialStartTime) {
+      this.initialStartTime = new Date();
+    }
+
+    this.leftBoobSubscription =
+      timer(0, 1000)
+        .pipe(timeInterval())
+        .subscribe(value => ++this.leftBoobElapsedSeconds)
+    this.leftBoobTimerOn = true
+    this.pauseRightBoobTimer()
+  }
+
+  pauseLeftBoobTimer(): void {
+    this.leftBoobSubscription?.unsubscribe()
+    this.leftBoobTimerOn = false
+
+    if (!this.rightBoobTimerOn) {
+      this.endTime = new Date();
+    }
+  }
+
+  startRightBoobTimer(): void {
+    if (!this.initialStartTime) {
+      this.initialStartTime = new Date();
+    }
+
+    this.rightBoobSubscription =
+      timer(0, 1000)
+        .pipe(timeInterval())
+        .subscribe(value => ++this.rightBoobElapsedSeconds)
+    this.rightBoobTimerOn = true
+    this.pauseLeftBoobTimer()
+  }
+
+  pauseRightBoobTimer(): void {
+    this.rightBoobSubscription?.unsubscribe()
+    this.rightBoobTimerOn = false
+
+    if (!this.leftBoobTimerOn) {
+      this.endTime = new Date();
+    }
+  }
 }
